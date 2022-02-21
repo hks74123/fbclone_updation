a192251a5604afe7aa0fe4d4db2f7478c52d571e
hks_74123 <56163188+hks74123@users.noreply.github.com>
2022-02-21 09:48:25 +0530

    Update settings.py

--- fbclone/settings.py
+++ fbclone/settings.py
@@ -136,7 +136,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.environ.get('REDIS_URL'), 'redis"//localhost:6379')],
+            "hosts": ["localhost", 6379],
         },
     },
 }
